691656602cc1104728ffbd8bba7fd62af3e4eaf2
Wayne Olson <weepler@Wayne-Olsons-MacBook-Pro.local>
2018-07-29 21:48:16 -0700

    adding a post model as illustrated in lesson 6 instructions

--- mysite/myblog/models.py
+++ mysite/myblog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrb.auth.models import User
 
-# Create your models here.
+'''
+Creating a post model from lesson 6 instructions
+'''
+
+
+class Post(models.Model):
+    title = models.Charfield(max_length=128)
+    text = models.TextField(blank=True)
+    author = models.ForeignKey(User)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(blank=True, null=True)
