6b585466eb2d5ce9035748fddb53b71f8473f457
Wayne Olson <weepler@D-10-19-253-159.dhcp4.washington.edu>
2018-07-30 19:36:13 -0700

    added def __str__(self): string into the categories class

--- mysite/myblog/models.py
+++ mysite/myblog/models.py
@@ -22,4 +22,7 @@ class Post(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField(blank=True)
-    posts = models.ManyToManyField(Post, blank=True, related_name = 'categories')
+    posts = models.ManyToManyField(Post, blank=True, related_name='categories')
+
+    def __str__(self):
+        return self.title
