6e482142e4de658cb87b820c518ed0bc317cae92
Wayne Olson <weepler@D-10-19-253-159.dhcp4.washington.edu>
2018-07-30 18:15:10 -0700

    all tests are finished

--- mysite/myblog/models.py
+++ mysite/myblog/models.py
@@ -7,9 +7,13 @@ Creating a post model from lesson 6 instructions
 
 
 class Post(models.Model):
+
     title = models.CharField(max_length=128)
     text = models.TextField(blank=True)
     author = models.ForeignKey(User, on_delete=models.PROTECT)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
     published_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
