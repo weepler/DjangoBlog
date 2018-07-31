9d1824b968bddc83364a047b2bdb28799387a10f
Wayne Olson <weepler@D-10-19-253-159.dhcp4.washington.edu>
2018-07-30 17:24:22 -0700

    editted tests.py to match the tests in lesson 6

--- mysite/myblog/tests.py
+++ mysite/myblog/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+
+class PostTestCase(TestCase):
+    fixtures = ['myblog_test_fixture.json',]
+
+    def setUp(self):
+        self.user = User.objects.get(pk=1)
 
 # Create your tests here.
