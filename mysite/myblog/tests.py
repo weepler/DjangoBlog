70202dab82add430b48097e8a85d074f206dab7e
Wayne Olson <weepler@D-10-19-253-159.dhcp4.washington.edu>
2018-07-30 19:33:28 -0700

    add category test case to tests.py

--- mysite/myblog/tests.py
+++ mysite/myblog/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from myblog.models import Post
+from myblog.models import Category
 
 class PostTestCase(TestCase):
     fixtures = ['myblog_test_fixture.json',]
@@ -13,5 +14,14 @@ class PostTestCase(TestCase):
         p1 = Post(title=expected)
         actual = str(p1)
         self.assertEqual(expected, actual)
+
+class CategoryTestCase(TestCase):
+
+    def test_string_representation(self):
+        expected = "A Category"
+        c1 = Category(name=expected)
+        actual = str(c1)
+        self.assertEqual(expected, actual)
+
 
 # Create your tests here.
