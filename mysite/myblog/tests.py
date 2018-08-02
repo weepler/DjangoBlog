eb51784db7f0cfba3b41ac282381d953893d58dc
Wayne Olson <weepler@Wayne-Olsons-MacBook-Pro.local>
2018-08-01 23:18:02 -0700

    finished the tests.py script

--- mysite/myblog/tests.py
+++ mysite/myblog/tests.py
@@ -43,5 +43,32 @@ class FrontEndTestCase(TestCase):
                 post.published_date = pubdate
             post.save()
 
+    def test_list_only_published(self):
+        resp = self.client.get('/')
+        # the content of the rendered response is always a bytestring 
+        resp_text = resp.content.decode(resp.charset)
+        self.assertTrue("Recent Posts" in resp_text)
+        for count in range (1,11):
+            title = "Post %d Title" % count
+            if count <6:
+                self.assertContains(resp, title, count=1)
+            else:
+                self.assertNotContains(resp, title)
+
+    def test_details_only_published(self):
+        for count in range(1, 11):
+            title = "Post %d Title" % count
+            post = Post.objects.get(title=title)
+            resp = self.client.get('/posts/%d/' % post.pk)
+            if count < 6:
+                self.assertEqual(resp.status_code, 200)
+                self.assertContains(resp, title)
+            else:
+                self.assertEqual(resp.status_code, 404)
+
+
+
+
+
 
 # Create your tests here.
